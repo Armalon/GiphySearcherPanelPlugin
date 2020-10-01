3c912973c36777915169870f3d0e8d2eeebe65a6
Armalon <armalong@gmail.com>
2020-10-01 13:38:15 +0200

    Showing loader on isLoading state

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -8,11 +8,13 @@ import GiphyLoader from './img/giphy_loader.svg'
 
 interface Props extends PanelProps<SimpleOptions> {}
 interface State {
+  isLoading: boolean;
   inputText: string;
 }
 
 class SimplePanel extends React.Component<Props, State> {
   public readonly state: Readonly<State> = {
+    isLoading: false,
     inputText: '',
   }
 
@@ -41,14 +43,14 @@ class SimplePanel extends React.Component<Props, State> {
               placeholder="Please enter keyword"
               value={this.state.inputText}
               onChange={this.handleTextChange} />
-            <button disabled>
+            <button disabled={!this.state.inputText.trim().length}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path></svg>
             </button>
           </form>
           <div className={styles.loader}>
-            <img src={GiphyLoader} className={css`
+            {this.state.isLoading ? <img src={GiphyLoader} className={css`
               height: ${this.props.height/4}px;
-            `} />
+            `} /> : ''}
           </div>
           <div className={styles.nothingFound}>Nothing found</div>
           <div className={styles.errorMessage}>
@@ -87,6 +89,14 @@ class SimplePanel extends React.Component<Props, State> {
 
   handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event && event.preventDefault()
+
+    if (this.state.isLoading) {
+      return
+    }
+
+    this.setState({
+      isLoading: true,
+    })
   }
 
   handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
