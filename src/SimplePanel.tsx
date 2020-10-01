9758bf7343605d33ca2b60d5b5395cba3310992f
Armalon <armalong@gmail.com>
2020-10-01 13:35:20 +0200

    Setting entered text into inputText

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -8,9 +8,14 @@ import GiphyLoader from './img/giphy_loader.svg'
 
 interface Props extends PanelProps<SimpleOptions> {}
 interface State {
+  inputText: string;
 }
 
 class SimplePanel extends React.Component<Props, State> {
+  public readonly state: Readonly<State> = {
+    inputText: '',
+  }
+
   render() {
     const styles = getStyles();
 
@@ -34,6 +39,7 @@ class SimplePanel extends React.Component<Props, State> {
             <input
               type="search"
               placeholder="Please enter keyword"
+              value={this.state.inputText}
               onChange={this.handleTextChange} />
             <button disabled>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path></svg>
@@ -84,7 +90,9 @@ class SimplePanel extends React.Component<Props, State> {
   }
 
   handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-
+    this.setState({
+      inputText: event.target.value
+    });
   }
 }
 
