984861d3822f3914e24d746226254f76227ec371
Armalon <armalong@gmail.com>
2020-10-01 10:20:31 +0200

    Transformed to a state based component

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,51 +2,33 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import {stylesFactory} from '@grafana/ui';
 
 interface Props extends PanelProps<SimpleOptions> {}
+interface State {
+}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
-  const styles = getStyles();
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
+class SimplePanel extends React.Component<Props, State> {
+  render() {
+    const styles = getStyles();
 
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
+    return (
+      <div
+        className={cx(
+          styles.wrapper,
+          css`
+            width: ${this.props.width}px;
+            height: ${this.props.height}px;
+          `
         )}
-        <div>Text option value: {options.text}</div>
+      >
+        TEST
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export { SimplePanel }
 
 const getStyles = stylesFactory(() => {
   return {
